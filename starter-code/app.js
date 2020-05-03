cff3443cd4a84a56babce63a3a8c8c364e16a9c0
CHRIS HAMERSLY <CHRISTOPHERHAMERSLY@GMAIL.COM>
2020-05-02 21:22:50 -0700

    Question1

--- starter-code/app.js
+++ starter-code/app.js
@@ -14,6 +14,8 @@ function sum(a, b) { //eslint-disable-line
   return [add, string];
 }
 
+sum (5,9);
+
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
 
@@ -30,10 +32,8 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
 
-}
-
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
